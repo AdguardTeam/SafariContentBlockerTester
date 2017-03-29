3bb4b94fff66eec3e15fb058c4877b98b39d4c81
Roman Sokolov <still2.mail@me.com>
2017-03-29 16:26:10 +0300

    update converter.js

--- compile.sh
+++ compile.sh
@@ -6,18 +6,18 @@ cd download
 
 mkdir filters
 
-curl -s -o 'filters/filter_1.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=1&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_2.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=2&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_3.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=3&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_4.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=4&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_5.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=5&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_6.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=6&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_7.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=7&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_8.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=8&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_9.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=9&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_10.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=10&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_11.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=11&key=4DDBE80A3DA94D819A00523252FB6380'
-curl -s -o 'filters/filter_12.txt' 'https://chrome.adtidy.org/getfilter.html?filterid=12&key=4DDBE80A3DA94D819A00523252FB6380'
+curl -s -o 'filters/filter_1.txt' 'https://filters.adtidy.org/ios/filters/1_optimized.txt'
+curl -s -o 'filters/filter_2.txt' 'https://filters.adtidy.org/ios/filters/2_optimized.txt'
+curl -s -o 'filters/filter_3.txt' 'https://filters.adtidy.org/ios/filters/3_optimized.txt'
+curl -s -o 'filters/filter_4.txt' 'https://filters.adtidy.org/ios/filters/4_optimized.txt'
+curl -s -o 'filters/filter_5.txt' 'https://filters.adtidy.org/ios/filters/5_optimized.txt'
+curl -s -o 'filters/filter_6.txt' 'https://filters.adtidy.org/ios/filters/6_optimized.txt'
+curl -s -o 'filters/filter_7.txt' 'https://filters.adtidy.org/ios/filters/7_optimized.txt'
+curl -s -o 'filters/filter_8.txt' 'https://filters.adtidy.org/ios/filters/8_optimized.txt'
+curl -s -o 'filters/filter_9.txt' 'https://filters.adtidy.org/ios/filters/9_optimized.txt'
+curl -s -o 'filters/filter_10.txt' 'https://filters.adtidy.org/ios/filters/10_optimized.txt'
+curl -s -o 'filters/filter_11.txt' 'https://filters.adtidy.org/ios/filters/11_optimized.txt'
+curl -s -o 'filters/filter_12.txt' 'https://filters.adtidy.org/ios/filters/12_optimized.txt'
 curl -s -o 'JSConverter.js' 'https://raw.githubusercontent.com/AdguardTeam/SafariContentBlockerConverterCompiler/master/compiled/JSConverter.js'
 
 cd ..
